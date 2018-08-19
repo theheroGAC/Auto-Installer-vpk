a449af64be866f1d49158b3de28712c0662d2f89
theheroGAC <gamesandconsolestaff@gmail.com>
2018-08-19 16:43:23 +0200

    v1.02

--- resources/autovpk.lua
+++ resources/autovpk.lua
@@ -1,8 +1,7 @@
 ------Injector
-		game.install("resources/VitaShell.vpk")
-              game.install("resources/Adrenaline.vpk")
-              game.install("resources/pkgj.vpk")
-              game.install("resources/Autoplugin.vpk")
+              onAppInstall("resources/Adrenaline.vpk")
+              onAppInstall("resources/pkgj.vpk")
+              onAppInstall("resources/Autoplugin.vpk")
 
 
 		os.message("Your PSVita will restart...\nThe vpk have been installed",0)
