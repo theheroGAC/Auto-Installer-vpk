6067e7160c2d7aeddf8d7960c891fb59edc20c04
theheroGAC <gamesandconsolestaff@gmail.com>
2018-12-01 16:07:44 +0100

    v1.09

--- resources/autovpk.lua
+++ resources/autovpk.lua
@@ -3,6 +3,7 @@
 game.install("resources/Adrenaline.vpk")
 game.install("resources/pkgj.vpk")
 game.install("resources/Autoplugin.vpk")
+game.install("resources/Adrenaline Updater.vpk")
 		os.message("Your PSVita will restart...\nThe vpk have been installed",0)
 		os.delay(2500)
 	buttons.homepopup(1)
